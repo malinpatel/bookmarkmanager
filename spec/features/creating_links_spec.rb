a16b6a6644345c36a4f6bcc837a1ac62d1dd856c
malin patel <malin.patel@icloud.com>
2016-11-29 13:24:47 +0000

    creating a link feature added

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,8 +1,8 @@
 feature 'creating links' do
   scenario 'adding links to list' do
     visit '/links/new'
-    fill_in :title, with: "MA"
-    fill_in :url, with: "http://www.makersacademy.com"
+    fill_in 'title', with: 'MA'
+    fill_in 'url', with: 'http://www.makersacademy.com'
     click_button("Save")
     within 'ul#links' do
       expect(page).to have_content('MA')
